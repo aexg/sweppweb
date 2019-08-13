3934a62fcfeec52c30e2c29ec0cd9a441d5690ff
aexg <29448170+aexg@users.noreply.github.com>
2019-08-13 22:11:49 +0200

    Updates

---
+++ precache-manifest.6e3f86bcf9e3b37318503e570e4e9ac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16edd6b56801af7a16fdd6ac020c67ff",
+    "url": "/sweppweb/index.html"
+  },
+  {
+    "revision": "304e2e21315b85dde8ef",
+    "url": "/sweppweb/static/css/main.a50b9a11.chunk.css"
+  },
+  {
+    "revision": "d39a9da0927e21d3f07d",
+    "url": "/sweppweb/static/js/2.4688b0c8.chunk.js"
+  },
+  {
+    "revision": "304e2e21315b85dde8ef",
+    "url": "/sweppweb/static/js/main.98a25691.chunk.js"
+  },
+  {
+    "revision": "702aa27dbfbf64c4500b",
+    "url": "/sweppweb/static/js/runtime~main.6f6973bb.js"
+  }
+]);
